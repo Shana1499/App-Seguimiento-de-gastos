fdfbeae6bd4841b71aeaa7b97156e6a09a3f563f
Mercedes Franchesca Gonzalez Cejas <mercedesgon03@gmail.com>
2023-06-27 10:25:54 +0200

    Added Navigation Drawer

--- app/src/main/java/com/example/appseguimientogastos/ui/compose/MainComposeApp.kt
+++ app/src/main/java/com/example/appseguimientogastos/ui/compose/MainComposeApp.kt
@@ -1,15 +1,48 @@
 package com.example.appseguimientogastos.ui.compose
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.appseguimientogastos.ui.compose.MainScreen.MainAppBar
-import com.example.appseguimientogastos.ui.compose.MainScreen.MainScreen
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.appseguimientogastos.R
+import com.example.appseguimientogastos.ui.compose.mainscreen.MainScreen
 import com.example.compose.AppSeguimientoGastosTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -17,22 +50,118 @@ import com.example.compose.AppSeguimientoGastosTheme
 fun MainComposeApp(
     modifier: Modifier = Modifier
 ) {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    val items = listOf(Icons.Default.Favorite, Icons.Default.Face, Icons.Default.Email)
+    val selectedItem = remember { mutableStateOf(items[0]) }
 
     AppSeguimientoGastosTheme {
         Surface(
             modifier = modifier.fillMaxSize()
         ) {
-            Scaffold(topBar = {
-                MainAppBar()
 
-            }) { innerPadding ->
-                MainScreen(modifier = modifier.padding(innerPadding))
-
-            }
-
-
+            ModalNavigationDrawer(
+                drawerState = drawerState,
+                drawerContent = {
+                    ModalDrawerSheet {
+                        Spacer(Modifier.height(12.dp))
+                        items.forEach { item ->
+                            NavigationDrawerItem(
+                                icon = { Icon(item, contentDescription = null) },
+                                label = { Text(item.name) },
+                                selected = item == selectedItem.value,
+                                onClick = {
+                                    scope.launch { drawerState.close() }
+                                    selectedItem.value = item
+                                },
+                                modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                            )
+                        }
+                    }
+                },
+                content = {
+                    Scaffold(
+                        topBar = {
+                            MainAppBar(drawerState, scope)
+                        }
+                    ) { innerPadding ->
+                        MainScreen(modifier = modifier.padding(innerPadding))
+                    }
+                }
+            )
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainAppBar(drawerState: DrawerState, scope: CoroutineScope) {
+    TopAppBar(
+        title = { Text(text = stringResource(id = R.string.app_name)) },
+        navigationIcon = {
+            IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                Icon(Icons.Default.Menu, contentDescription = "Menu")
+            }
+        },
+
+        )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomNavigationDrawer(drawerState: DrawerState, scope: CoroutineScope) {
+// icons to mimic drawer destinations
+    val items = listOf(Icons.Default.Favorite, Icons.Default.Face, Icons.Default.Email)
+    val selectedItem = remember { mutableStateOf(items[0]) }
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Spacer(Modifier.height(12.dp))
+                items.forEach { item ->
+                    NavigationDrawerItem(
+                        icon = { Icon(item, contentDescription = null) },
+                        label = { Text(item.name) },
+                        selected = item == selectedItem.value,
+                        onClick = {
+                            scope.launch { drawerState.close() }
+                            selectedItem.value = item
+                        },
+                        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                    )
+                }
+            }
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = if (drawerState.isClosed) ">>> Swipe >>>" else "<<< Swipe <<<")
+                Spacer(Modifier.height(20.dp))
+                Button(onClick = { scope.launch { drawerState.open() } }) {
+                    Text("Click to open")
+                }
+            }
+        }
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun MainComposeAppPreview() {
+    MainComposeApp()
+
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun MainComposeAppDarkPreview() {
+    MainComposeApp()
+
+}
 
--- app/src/main/java/com/example/appseguimientogastos/ui/compose/mainscreen/MainScreen.kt
+++ app/src/main/java/com/example/appseguimientogastos/ui/compose/mainscreen/MainScreen.kt
@@ -1,28 +1,24 @@
-package com.example.appseguimientogastos.ui.compose.MainScreen
+package com.example.appseguimientogastos.ui.compose.mainscreen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
@@ -30,11 +26,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.appseguimientogastos.R
+import com.example.appseguimientogastos.ui.compose.MainAppBar
 import com.example.appseguimientogastos.ui.compose.MainComposeApp
+import com.example.compose.AppSeguimientoGastosTheme
 import com.example.compose.md_theme_light_Ahorro
 import com.example.compose.md_theme_light_gastos
 import com.example.compose.md_theme_light_ingreso
@@ -59,40 +56,6 @@ fun MainScreen(modifier: Modifier = Modifier) {
 
     }
 }
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainAppBar(modifier: Modifier = Modifier) {
-    CenterAlignedTopAppBar(
-        title = {
-            Text(
-                stringResource(id = R.string.app_name),
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                style = MaterialTheme.typography.displaySmall
-            )
-        },
-        navigationIcon = {
-            var showMenu by remember { mutableStateOf(false) }
-            IconButton(onClick = { showMenu = !showMenu }) {
-                Icon(Icons.Default.Menu, contentDescription = "Menu")
-            }
-            DropdownMenu(
-                expanded = showMenu,
-                onDismissRequest = { showMenu = false }
-            ) {
-                DropdownMenuItem(text = { Text("Menu Item 1") }, onClick = { /*TODO*/ })
-                DropdownMenuItem(text = { Text("Menu Item 2") }, onClick = { /*TODO*/ })
-                DropdownMenuItem(text = { Text("Menu Item 3") }, onClick = { /*TODO*/ })
-                DropdownMenuItem(text = { Text("Menu Item 4") }, onClick = { /*TODO*/ })
-                DropdownMenuItem(text = { Text("Menu Item 5") }, onClick = { /*TODO*/ })
-            }
-        },
-
-        )
-
-}
-
 
 /**
  * Card de la Dashboard
@@ -199,7 +162,7 @@ fun InfoProgressbar(
     titleList: List<Int>,
     colorList: List<Color>
 ) {
-    progressList.forEachIndexed { index, progress ->
+    progressList.forEachIndexed { index, _ ->
         Row(modifier.padding(bottom = dimensionResource(id = R.dimen.default_smallpadding))) {
             Canvas(
                 modifier = Modifier
@@ -297,16 +260,14 @@ fun OverviewCard(modifier: Modifier = Modifier, title: String) {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun MainComposeAppPreview() {
     MainComposeApp()
-
 }
-
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun MainComposeAppDarkPreview() {
     MainComposeApp()
-
 }
